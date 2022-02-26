b96dec75c6b6008cc0fa71715f4b3ce37b549d67
ethanwu <wue1@wit.edu>
2022-02-26 01:06:11 -0500

    Added rel attribute

--- src/IconBar.js
+++ src/IconBar.js
@@ -11,13 +11,25 @@ const IconBar = ({ toggleEmailBar }) => {
   return (
     <nav className="social_bar">
       <FaInbox className="icons" onClick={() => toggleEmailBar()} />
-      <a href="https://github.com/wue1atwit" target={"_blank"}>
+      <a
+        href="https://github.com/wue1atwit"
+        target={"_blank"}
+        rel={"noreferrer"}
+      >
         <FaGithub className="icons" />
       </a>
-      <a href="https://www.linkedin.com/in/ethanwu13/" target={"_blank"}>
+      <a
+        href="https://www.linkedin.com/in/ethanwu13/"
+        target={"_blank"}
+        rel={"noreferrer"}
+      >
         <FaLinkedin className="icons" />
       </a>
-      <a href="https://www.instagram.com/ethanwu13/" target={"_blank"}>
+      <a
+        href="https://www.instagram.com/ethanwu13/"
+        target={"_blank"}
+        rel={"noreferrer"}
+      >
         <FaInstagram className="icons" />
       </a>
       <FaCog className="icons" />
