d6f2bdac2ecbde0c4f70f2cfac4491b65f97d24e
ethanwu <wue1@wit.edu>
2022-02-26 02:49:23 -0500

    Changed email tabs and added attachment icon

--- src/emailComp/EmailTab.js
+++ src/emailComp/EmailTab.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { FaPaperclip } from "react-icons/fa";
 
 const EmailTab = ({
   id,
   from,
   date,
   subject,
+  hasAttachment,
   greeting,
   message,
   selectMsg,
@@ -18,11 +20,15 @@ const EmailTab = ({
         toggleEmailBar();
       }}
     >
-      <h2 className="froms">{from}</h2>
-      <div className="rItems">
+      <div className="tab_header">
+        <p className="froms">{from}</p>
+        <div className="th_icons">{hasAttachment && <FaPaperclip />}</div>
+      </div>
+
+      <div className="tab_subheader">
+        <p className="subject">{subject}</p>
         <p className="date">{date}</p>
       </div>
-      <p className="subject">{subject}</p>
       <p className="preview">{`${greeting} ${message.substring(0, 100)}...`}</p>
     </article>
   );
--- src/res/emailData.js
+++ src/res/emailData.js
@@ -6,6 +6,7 @@ const data = [
     sDate: "Mon 12/20",
     time: "1:24 pm",
     subject: "About Me",
+    hasAttachment: false,
     greeting: "Hello,",
     message:
       "I am Ethan Wu, a Sophomore studying in Computer Science at Wentworth Institute of Technology.",
@@ -17,6 +18,8 @@ const data = [
     sDate: "Mon 12/20",
     time: "XX:XX pm",
     subject: "Resume",
+    hasAttachment: true,
+    greeting: "Hello,",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorum vitae tenetur optio repudiandae commodi pariatur id possimus aut error quasi consectetur ex est nihil nisi quibusdam iusto quidem saepe harum, doloribus deleniti? In illo est ipsam voluptas ipsa inventore aspernatur impedit ullam facere sit maxime, officiis possimus minus doloribus asperiores maiores ad repellendus esse, molestias cumque nostrum nobis. Pariatur ipsam accusamus maiores animi, fuga eligendi iure quisquam error, deleniti exercitationem, laborum inventore ipsa. Nulla assumenda, aut blanditiis saepe dicta voluptate dolores nam, corporis expedita ea, itaque porro ipsum inventore? Accusantium voluptatibus modi dolorem impedit eligendi tempore eius sequi repellendus.",
   },
@@ -27,6 +30,8 @@ const data = [
     sDate: "Mon 12/20",
     time: "XX:XX pm",
     subject: "Project1",
+    hasAttachment: false,
+    greeting: "Hello,",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorum vitae tenetur optio repudiandae commodi pariatur id possimus aut error quasi consectetur ex est nihil nisi quibusdam iusto quidem saepe harum, doloribus deleniti? In illo est ipsam voluptas ipsa inventore aspernatur impedit ullam facere sit maxime, officiis possimus minus doloribus asperiores maiores ad repellendus esse, molestias cumque nostrum nobis. Pariatur ipsam accusamus maiores animi, fuga eligendi iure quisquam error, deleniti exercitationem, laborum inventore ipsa. Nulla assumenda, aut blanditiis saepe dicta voluptate dolores nam, corporis expedita ea, itaque porro ipsum inventore? Accusantium voluptatibus modi dolorem impedit eligendi tempore eius sequi repellendus.",
   },
