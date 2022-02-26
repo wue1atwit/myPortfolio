70a735fc7e138d37738ddcee006ea7dd69f8436d
ethanwu <wue1@wit.edu>
2022-02-26 00:48:50 -0500

    Icons go to new tab

--- src/IconBar.js
+++ src/IconBar.js
@@ -11,13 +11,13 @@ const IconBar = ({ toggleEmailBar }) => {
   return (
     <nav className="social_bar">
       <FaInbox className="icons" onClick={() => toggleEmailBar()} />
-      <a href="https://github.com/wue1atwit">
+      <a href="https://github.com/wue1atwit" target={"_blank"}>
         <FaGithub className="icons" />
       </a>
-      <a href="https://www.linkedin.com/in/ethanwu13/">
+      <a href="https://www.linkedin.com/in/ethanwu13/" target={"_blank"}>
         <FaLinkedin className="icons" />
       </a>
-      <a href="https://www.instagram.com/ethanwu13/">
+      <a href="https://www.instagram.com/ethanwu13/" target={"_blank"}>
         <FaInstagram className="icons" />
       </a>
       <FaCog className="icons" />
