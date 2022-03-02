e8bfcb12a13c91ff7fe61de5e519e69d5b00ba19
ethanwu <wue1@wit.edu>
2022-03-02 01:39:54 -0500

    Edit dates/time and added messages

--- src/res/emailData.js
+++ src/res/emailData.js
@@ -7,9 +7,8 @@ const data = [
     time: "11:24 pm",
     subject: "About Me",
     hasAttachment: false,
-    greeting: "Hello,",
     message:
-      "My name is Ethan Wu and I am Sophomore studying Computer Science at Wentworth Institute of Technology. My interest in coding stemmed from when I was about 12 or 13, when I attempted to create a Minecraft mod. Although I was not successful, I was able to learn about the process of coding and beneficial coding syntaxes. As I reached high school, I was enrolled in my school's robotic program, where I was able to build a robot and program how it function. I found it exciting to see how the robot ",
+      "Welcome to my web portfolio, \nMy name is Ethan and I am Sophomore studying Computer Science at Wentworth Institute of Technology. My interest in coding stemmed from when I was younger, when I attempted to create a Minecraft mod using Java. Although I was not successful, I was able to quickly pickup on the process of coding and the beneficial coding syntaxes. \nAs I reached high school, I was enrolled in my school's robotic program, where I was able to program how a robot and a vending machine functioned. This experience piqued my interest in programming because of how exhilarating it was to see my program function outside the scope of just code. I strive to continue making programs that displays my creativity and passion in what I learn. \n\nBest,\nEthan Wu",
   },
   {
     id: 1,
@@ -19,9 +18,8 @@ const data = [
     time: "5:44 pm",
     subject: "Resume",
     hasAttachment: true,
-    greeting: "Hello,",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorum vitae tenetur optio repudiandae commodi pariatur id possimus aut error quasi consectetur ex est nihil nisi quibusdam iusto quidem saepe harum, doloribus deleniti? In illo est ipsam voluptas ipsa inventore aspernatur impedit ullam facere sit maxime, officiis possimus minus doloribus asperiores maiores ad repellendus esse, molestias cumque nostrum nobis. Pariatur ipsam accusamus maiores animi, fuga eligendi iure quisquam error, deleniti exercitationem, laborum inventore ipsa. Nulla assumenda, aut blanditiis saepe dicta voluptate dolores nam, corporis expedita ea, itaque porro ipsum inventore? Accusantium voluptatibus modi dolorem impedit eligendi tempore eius sequi repellendus.",
+      'Hello, \nAttached in this email is my resume. Feel free to download and view my resume as you please. Do not hesistate to contact me if you have any questions. You can contact me via the "plus/compose" button on the bottom right.  \n\nSincerely, \nEthan Wu',
   },
   {
     id: 2,
@@ -31,7 +29,6 @@ const data = [
     time: "2:57 am",
     subject: "Project1",
     hasAttachment: false,
-    greeting: "Hello,",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorum vitae tenetur optio repudiandae commodi pariatur id possimus aut error quasi consectetur ex est nihil nisi quibusdam iusto quidem saepe harum, doloribus deleniti? In illo est ipsam voluptas ipsa inventore aspernatur impedit ullam facere sit maxime, officiis possimus minus doloribus asperiores maiores ad repellendus esse, molestias cumque nostrum nobis. Pariatur ipsam accusamus maiores animi, fuga eligendi iure quisquam error, deleniti exercitationem, laborum inventore ipsa. Nulla assumenda, aut blanditiis saepe dicta voluptate dolores nam, corporis expedita ea, itaque porro ipsum inventore? Accusantium voluptatibus modi dolorem impedit eligendi tempore eius sequi repellendus.",
   },
