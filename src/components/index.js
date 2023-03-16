c233a31c0bc030bfb5182aa903dbfe7aa3af08ef
ethanwu <wue1@wit.edu>
2023-03-15 20:05:02 -0700

    Component index file

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import EmailListItem from "./EmailListItem";
+import EmailMessage from "./EmailMessage";
+import AttachmentItem from "./AttachmentItem";
+import Container from "./Container";
+import QuickReply from "./QuickReply";
+
+export { EmailListItem, EmailMessage, AttachmentItem, Container, QuickReply };
