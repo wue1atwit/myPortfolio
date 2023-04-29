1727a93e5213896614330ea075fe5b389a885e76
ethanwu <wue1@wit.edu>
2023-04-28 19:38:16 -0700

    Fixed wrap and text overflow issue

--- src/components/EmailListItem.js
+++ src/components/EmailListItem.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./EmailListItem.scss";
 import { FaPaperPlane, FaPaperclip } from "react-icons/fa";
 
-const EmailListItem = ({ subject, preview, date }) => {
+const EmailListItem = ({ subject, message, date }) => {
   return (
     <div className="email-item">
       <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
         <div className="placeholder-image">
           <p className="E">E</p>
         </div>
-        <div>
+        <div className="item-text-container">
           <p className="item-text">Ethan Wu</p>
           <p className="item-text">{subject}</p>
-          <p className="item-text">{preview}</p>
+          <p className="item-text preview">{message}</p>
           <div
             style={{
               marginTop: "4px",
