d75b2de5b14f41e3eb12251aee05ff3c318d531e
ethanwu <wue1@wit.edu>
2023-06-07 11:08:06 -0400

    Functional tinker component

--- src/screens/Tinker.js
+++ src/screens/Tinker.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./Tinker.scss";
+import { FaCog } from "react-icons/fa";
 
 const Tinker = () => {
   return (
-    <div>
-      Maintanence: My portfolio is only available for screen size 1056px or
-      greater.
+    <div className="tinker-container">
+      <FaCog className="cog" />
+      <span className="head">Maintanence</span>
+      <span className="body">
+        My portfolio is only available for screen widths 1056px or greater.
+      </span>
     </div>
   );
 };
