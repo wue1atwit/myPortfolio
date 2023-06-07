4267681f2fd479dad8b4049c876bfdf5a27cd00f
ethanwu <wue1@wit.edu>
2023-06-07 10:04:36 -0400

    Initial screens folder

---
+++ src/screens/Tinker.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Tinker = () => {
+  return (
+    <div>
+      Maintanence: My portfolio is only available for screen size 1056px or
+      greater.
+    </div>
+  );
+};
+
+export default Tinker;
